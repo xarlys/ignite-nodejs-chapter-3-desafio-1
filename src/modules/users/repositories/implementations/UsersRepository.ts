81b7b0f6b9f98a3fb1487c87e1acc20427286402
xarlys <xarlysouza@hotmail.com>
2021-03-27 12:39:37 -0300

    commit desafio finalizado

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -15,16 +15,25 @@ export class UsersRepository implements IUsersRepository {
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
     // Complete usando ORM
+    const user = await this.repository.findOne({
+      where: {
+        id:user_id,
+      },
+      relations: ["games"]
+    });
+    return user as User
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return await this.repository.query('SELECT * FROM "users" ORDER BY "first_name" ASC'); 
+    // Complete usando raw query
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query('SELECT * FROM "users" WHERE (lower("first_name") = lower($1)) AND (lower("last_name") = lower($2))', [first_name, last_name]); 
+    // Complete usando raw query
   }
 }
